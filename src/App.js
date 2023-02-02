832e99a3dffa24467b937cec108b38ec6b531e4d
Deepak <0822deepakverma@gmail.com>
2023-02-02 22:47:20 +0530

    recursion added

--- src/App.js
+++ src/App.js
@@ -22,32 +22,34 @@ const App = () => {
     });
   };
 
+  const createDropdowns = (currentOption, index) => {
+    if (!currentOption || !currentOption.length) {
+      return null;
+    }
+    return (
+      <Dropdown
+        key={index}
+        options={currentOption}
+        value={selectedValues[index]}
+        onChange={(event) => handleChange(index, event)}
+      />
+    );
+  };
+
+  const renderDropdowns = (selectedValues, currentOption) => {
+    return selectedValues.reduce((acc, selectedValue, index) => {
+      const currentOption = selectedValues
+        .slice(0, index)
+        .reduce((currentOption, selectedValue) => currentOption[selectedValue].struct, data.struct);
+      return [...acc, createDropdowns(currentOption, index)];
+    }, [createDropdowns(currentOption, selectedValues.length)]);
+  };
+
   const currentOption = selectedValues.reduce((currentOption, selectedValue) => {
     return currentOption[selectedValue].struct;
   }, data.struct);
 
-  return (
-    <div>
-      {selectedValues.map((selectedValue, index) => (
-        <Dropdown
-          key={index}
-          options={
-            selectedValues
-              .slice(0, index)
-              .reduce((currentOption, selectedValue) => currentOption[selectedValue].struct, data.struct)
-          }
-          value={selectedValue}
-          onChange={(event) => handleChange(index, event)}
-        />
-      ))}
-      {currentOption.length > 0 && (
-        <Dropdown
-          options={currentOption}
-          onChange={(event) => handleChange(selectedValues.length, event)}
-        />
-      )}
-    </div>
-  );
+  return <div>{renderDropdowns(selectedValues, currentOption)}</div>;
 };
 
 export default App;
