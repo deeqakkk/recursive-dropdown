0e85b64b65defc642413b29db48a78502db9ce29
Deepak <0822deepakverma@gmail.com>
2023-02-02 22:35:24 +0530

    optimised

--- src/App.js
+++ src/App.js
@@ -1,64 +1,49 @@
 import React, { useState } from 'react';
 import data from './data.json';
 
-const Dropdown = ({ options, onChange, value }) => {
-  return (
-    <select value={value || ''} onChange={onChange}>
-      {options.map((option, i) => (
-        <option key={i} value={i}>
-          {option.name}
-        </option>
-      ))}
-    </select>
-  );
-};
+const Dropdown = ({ options, onChange, value }) => (
+  <select value={value || ''} onChange={onChange}>
+    {options.map((option, i) => (
+      <option key={i} value={i}>
+        {option.name}
+      </option>
+    ))}
+  </select>
+);
 
 const App = () => {
-  const [selectedValue, setSelectedValue] = useState(null);
-  const [foundStruct, setFoundStruct] = useState(null);
-  const [foundStruct2, setFoundStruct2] = useState(null);
+  const [selectedValues, setSelectedValues] = useState([]);
 
-  const handleChange = (level, event) => {
-    const selectedOption = (level === 1)
-      ? data.struct[event.target.value]
-      : foundStruct[event.target.value];
-
-    if (level === 1) {
-      setSelectedValue(event.target.value);
-    }
-
-    if (selectedOption.struct) {
-      if (level === 1) {
-        setFoundStruct(selectedOption.struct);
-      } else {
-        setFoundStruct2(selectedOption.struct);
-      }
-    } else {
-      if (level === 1) {
-        setFoundStruct(null);
-      } else {
-        setFoundStruct2(null);
-      }
-    }
+  const handleChange = (index, event) => {
+    setSelectedValues((prevSelectedValues) => {
+      const newSelectedValues = [...prevSelectedValues];
+      newSelectedValues[index] = event.target.value;
+      return newSelectedValues;
+    });
   };
+
+  const currentOption = selectedValues.reduce((currentOption, selectedValue) => {
+    return currentOption[selectedValue].struct;
+  }, data.struct);
 
   return (
     <div>
-      <Dropdown
-        options={data.struct}
-        value={selectedValue}
-        onChange={(event) => handleChange(1, event)}
-      />
-      {foundStruct && (
+      {selectedValues.map((selectedValue, index) => (
         <Dropdown
-          options={foundStruct}
-          onChange={(event) => handleChange(2, event)}
+          key={index}
+          options={
+            selectedValues
+              .slice(0, index)
+              .reduce((currentOption, selectedValue) => currentOption[selectedValue].struct, data.struct)
+          }
+          value={selectedValue}
+          onChange={(event) => handleChange(index, event)}
         />
-      )}
-      {foundStruct2 && (
+      ))}
+      {currentOption.length > 0 && (
         <Dropdown
-          options={foundStruct2}
-          onChange={() => {}}
+          options={currentOption}
+          onChange={(event) => handleChange(selectedValues.length, event)}
         />
       )}
     </div>
