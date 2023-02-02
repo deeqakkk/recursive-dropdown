d68aac34399fde81e1dc2a9fd1a07e49295f4497
Deepak <0822deepakverma@gmail.com>
2023-02-02 22:19:34 +0530

    dropdown added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data.json';
 
-function App() {
+const Dropdown = ({ options, onChange, value }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <select value={value || ''} onChange={onChange}>
+      {options.map((option, i) => (
+        <option key={i} value={i}>
+          {option.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+const App = () => {
+  const [selectedValue, setSelectedValue] = useState(null);
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <Dropdown
+        options={data.struct}
+        value={selectedValue}
+        onChange={handleChange}
+      />
     </div>
   );
-}
+};
 
 export default App;
